211123054a9616c92e9511f2bb2c6cc070d29310
nahoor <45914426+nahoor@users.noreply.github.com>
2023-09-14 23:29:12 +0530

    Safe.js

--- Safe.js
+++ Safe.js
@@ -1,67 +1,42 @@
 function extractDomain(url) {
-	var hostname;
-	if (url.indexOf("://") > -1) {
-		hostname = url.split('/')[2];
-	} else {
-		hostname = url.split('/')[0];
-	}
-	hostname = hostname.split(':')[0];
-	hostname = hostname.split('?')[0];
-	return hostname;
+    var hostname;
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    } else {
+        hostname = url.split('/')[0];
+    }
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+    return hostname;
 }
 
-function exception() {
-	var exception = new Array();
-	setting.exceptionurl = setting.exceptionurl;
-	exception = setting.exceptionurl.split(",");
-	return exception;
+function encodeBase64(str) {
+    return btoa(str);
 }
 
-if (!setting.exceptionurl) {
-	setting.exceptionurl = window.location.href;
-} else {
-	setting.exceptionurl += "," + window.location.href;
+function decodeBase64(str) {
+    return atob(str);
 }
-var exception = exception();
 
 function showurl(datajson) {
+    var linktag = document.getElementsByTagName("a");
+    var links = [];
 
-	var check = false;
-	var no = 0;
-	var exceptionlength = exception.length;
-	var checklink = "";
-	var checkexception = "";
-	var linktag = document.getElementsByTagName("a");
-	var links = new Array();
+    var semuaartikel = datajson.feed.openSearch$totalResults.$t;
+    for (var i = 0; i < semuaartikel; i++) {
+        var urlartikel;
+        for (var s = 0; s < datajson.feed.entry[i].link.length; s++) {
+            if (datajson.feed.entry[i].link[s].rel == 'alternate') {
+                urlartikel = datajson.feed.entry[i].link[s].href;
+                break;
+            }
+        }
+        links[i] = urlartikel;
+    }
 
-	var semuaartikel = datajson.feed.openSearch$totalResults.$t;
-	for (var i = 0; i < semuaartikel; i++) {
-		var urlartikel;
-		for (var s = 0; s < datajson.feed.entry[i].link.length; s++) {
-			if (datajson.feed.entry[i].link[s].rel == 'alternate') {
-				urlartikel = datajson.feed.entry[i].link[s].href;
-				break;
-			}
-		}
-		links[i] = urlartikel;
-		var randindex = Math.random() * links.length;
-		randindex = parseInt(randindex);
-	}
-	for (var i = 0; i < linktag.length; i++) {
-		check = false;
-		no = 0;
-		while (check == false && no < exceptionlength) {
-			checklink = extractDomain(linktag[i].href);
-			checkexception = extractDomain(exception[no]);
-			if (checklink.match(checkexception)) {
-				check = true;
-			}
-			no++;
-		}
-		if (check == false) {
-			linktag[i].href = links[randindex] + setting.path + btoa(convertstr(linktag[i].href));
-			linktag[i].rel = "nofollow";
-			linktag[i].target = "_blank";
-		}
-	}
+    for (var i = 0; i < linktag.length; i++) {
+        linktag[i].href = encodeBase64(links[i % links.length]);
+        linktag[i].rel = "nofollow";
+        linktag[i].target = "_blank";
+    }
 }
